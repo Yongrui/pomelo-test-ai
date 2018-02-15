b0c6b3c6285a370c263fa19ab92b7d274f19a407
Yongrui <liangyongrui@gmail.com>
2018-02-15 13:10:08 +0800

    fix entity stand action

--- game-server/app/ai/action/stand.js
+++ game-server/app/ai/action/stand.js
@@ -13,26 +13,25 @@ module.exports = Action;
 var pro = Action.prototype;
 
 pro.doAction = function() {
-	utils.myPrint('1 ~ stand');
 	var character = this.blackboard.curCharacter;
 	var targetId = this.blackboard.curTarget;
 	if (!!targetId) {
 		return bt.RES_SUCCESS;
 	}
+	utils.myPrint('1 ~ stand ', character.entityId);
 
 	if (!this.blackboard.stand) {
 		this.time = Date.now();
-		utils.myPrint('3 ~ stand ');
 		if (this.blackboard.moved) {
-			utils.myPrint('4 ~ stand ');
 			this.blackboard.arena.timer.abortAction('move', character.entityId);
+			this.blackboard.moved = false;
 		}
 		character.stand();
 		this.blackboard.stand = true;
 	}
 	else {
 		var time = Date.now() - this.time;
-		if (time > 1000) {
+		if (time >= 1000) {
 			this.blackboard.stand = false;
 			return bt.RES_FAIL;
 		}
